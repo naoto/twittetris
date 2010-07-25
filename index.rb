0892964e71b0882fb5f9fa8e236ec9ae1ffa49b7
Naoto SHINGAKI <nao.sora+github@gmail,com>
2010-07-25 23:18:23 +0900

    play

--- index.rb
+++ index.rb
@@ -3,6 +3,35 @@
 require 'rubygems'
 require 'sinatra'
 require 'twitter/twitter.rb'
+require 'sequel'
+require 'logger'
+
+DB = Sequel.sqlite('strage.db', :loggers => [Logger.new($stdout)])
+enable :sessions
+
+configure do
+#  DB.create_table :users do
+#    primary_key :id
+#    String :name
+#    String :oauth_token
+#    String :oauth_verifie
+#    String :request_token
+#    String :request_token_secret
+#    String :access_token
+#    String :access_token_secret
+#  end
+#    DB.create_table :followers do
+#      primary_key :id
+#      String :name
+#      String :screen
+#      String :image
+#    end
+#  DB.create_table :matchers do
+#    primary_key :id
+#    String :user_id
+#    String :followers_id
+#  end
+end
 
 get '/' do
  erb :index 
@@ -10,7 +39,64 @@ end
 
 post '/login' do
   
-  twitter = TwitterOauth.new(params[:id], params[:pass])
-  twitter.registry()
+  id = params[:id]
+  redirect "/#{id}" unless DB[:users].where(:name => id).empty?
+
+  twitter = TwitterOauth.new
+  req_token = twitter.registry(id)
+
+  session["userid"] = id
+  session["request_token"] = req_token.token
+  session["request_token_secret"] = req_token.secret
+  redirect req_token.authorize_url
   
+end
+
+get '/init' do
+  oauth_token = params["oauth_token"]
+  oauth_vrfy = params["oauth_verifier"]
+  req_token = session["request_token"]
+  req_token_secret = session["request_token_secret"]  
+  
+  twitter = TwitterOauth.new
+  access = twitter.access_token(oauth_vrfy, req_token, req_token_secret)
+
+  users = DB[:users].insert(
+                       :name => session["userid"],
+		       :oauth_token => oauth_token,
+		       :oauth_verifie => oauth_vrfy,
+		       :request_token => req_token,
+		       :request_token_secret => req_token_secret,
+		       :access_token => access.token,
+		       :access_token_secret => access.secret
+  )
+  user = DB[:users].first(:name => session["userid"])
+
+  twitter.auth(access.token, access.secret)
+
+  users = twitter.followers(session["userid"])
+  users.each{ |twit|
+    follower = DB[:followers].first( :name => twit["screen_name"])
+    if follower.nil?
+      follower = DB[:followers].insert(
+	      :name => twit["screen_name"],
+	      :screen => twit["name"],
+	      :image => twit["profile_image_url"]
+      )
+      follower = DB[:followers].first( :name => twit["screen_name"])
+    end
+    DB[:matchers].insert(
+	    :user_id => user[:id],
+	    :followers_id => follower[:id]
+    )
+  }
+  redirect "/#{session["userid"]}"
+end
+
+get '/:name' do
+  @user = DB[:users].join(:matchers, :user_id => :id).join(
+          :followers, :id => :followers_id).filter("users.name = ?", params[:name])
+  redirect "/" if @user.nil?
+  
+  erb :tetris
 end
--- twitter/twitter.rb
+++ twitter/twitter.rb
@@ -1,6 +1,3 @@
-#!/usr/bin/env ruby
-
-require 'twitter/auth.rb'
 require 'rubygems'
 require 'oauth'
 require 'yaml'
@@ -12,27 +9,48 @@ class TwitterOauth
 
   ACCOUNT_YAML = "twitter/.account.yaml"
 
-  def initialize(id, pass)
-    @account = {:id => id, :password => pass} 
+  def initialize
     @yaml = YAML.load_file(ACCOUNT_YAML)
   end
 
-  def registry
-    cgi = CGI.new
-    session = CGI::Session.new(
-      cgi,{"new_session" => true, "tmpdir" => "./work/"}
+  def access_token(vrfy, req_token, secret)
+    consumer = OAuth::Consumer.new(
+      @yaml["consumer_key"], @yaml["consumer_secret"],
+      {:site => "http://twitter.com"}
     )
 
+    request_token = OAuth::RequestToken.new(
+      consumer,
+      req_token,
+      secret
+    )
+   
+    prm = { :oauth_verifier => vrfy } 
+    request_token.get_access_token(prm)
+  end
+
+  def registry(id)
     consumer = OAuth::Consumer.new(
       @yaml["consumer_key"], @yaml["consumer_secret"],
       {:site => "http://twitter.com"}
     )
 
     optprm = { :oauth_callback => "http://naotos.ddo.jp/init" }
-    rquest_token = consumer.get_request_token(optprm, {})
-    session["request_token"] = request_token.token
-    session["request_token_secret"] = request_token.secret
-    print cgi.header( 'Location' => request_token.authorize_url)
+    consumer.get_request_token(optprm, {})
+
+  end
+
+  def auth(access_token, token_secret)
+    @consumer = OAuth::Consumer.new(
+      @yaml["consumer_key"], @yaml["consumer_secret"],
+      {:site => "http://twitter.com"}
+    )
+
+    @access_token = OAuth::AccessToken.new(
+      @consumer,
+      access_token,
+      token_secret
+    )
   end
 
   def post(word)
@@ -44,12 +62,19 @@ class TwitterOauth
     p e
   end
 
-  def followers(page = 0)
-    response = @access_token.get(
-      'http://twitter.com/statuses/followers.json'
-    )
+  def followers(name)
+   
+    user = []
+    cursor = "-1"
 
-   JSON.parse(response.body)
+    #while true do
+      response = @access_token.get(
+        "http://twitter.com/statuses/followers/#{name}.json",
+        {'cursor' => cursor, 'lite' => "true"}
+      )
+     JSON.parse(response.body)
+    #end
+
   rescue => e
     p e
   end
